8cce88dad1179d03726a577889720eada3a9865b
commixon <commixon@gmail.com>
2014-05-30 20:18:32 +0300

    Add newcly created client files

---
+++ mist/client.py
@@ -0,0 +1,235 @@
+import json
+
+from mist.helpers import RequestsHandler
+from mist.helpers import parse_conf_file
+
+
+class MistClient(object):
+    """
+
+    """
+    def __init__(self, mist_uri="https://mist.io", email=None, password=None):
+        self.uri = mist_uri
+        self.email = email
+        self.password = password
+        self.api_token = None
+        self.user_details = None
+
+        self.__init_configure()
+
+        if self.email and self.password:
+            self.__authenticate()
+
+    def __init_configure(self):
+        if (not self.email) and (not self.password):
+            credentials = parse_conf_file()
+            self.email = credentials['EMAIL']
+            self.password = credentials['PASSWORD']
+
+    def __authenticate(self):
+        payload = {
+            'email': self.email,
+            'password': self.password
+        }
+
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/auth', data=data)
+        response = req.post().json()
+        token = response['mist_api_token']
+        self.api_token = "mist_1 %s:%s" % (self.email, token)
+        self.user_details = response
+
+    def supported_providers(self):
+        req = RequestsHandler(self.uri+'/providers', api_token=self.api_token)
+        providers = req.get().json()
+        supported_providers = providers['supported_providers']
+        return supported_providers
+
+    def list_backends(self):
+        req = RequestsHandler(self.uri+'/backends', api_token=self.api_token)
+        backends = req.get().json()
+        return backends
+
+    def add_backend(self, title, provider, key, secret, tenant_name=None, region=None, apiurl=None, machine_ip=None,
+                    machine_key=None, machine_user=None, compute_endpoint=None, machine_port=None):
+        payload = {
+            'title': title,
+            'provider': provider,
+            'apikey': key,
+            'apisecret': secret,
+            'tenant_name': tenant_name,
+            'region': region,
+            'apiurl': apiurl,
+            'machine_ip': machine_ip,
+            'machine_key': machine_key,
+            'machine_user': machine_user,
+            'compute_endpoint': compute_endpoint,
+            'machine_port': machine_port
+        }
+
+        req = RequestsHandler(self.uri+'/backends', data=json.dumps(payload), api_token=self.api_token)
+        response = req.post()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def delete_backend(self, backend_id):
+        req = RequestsHandler(self.uri+'/backends/'+backend_id, api_token=self.api_token)
+        response = req.delete()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def rename_backend(self, backend_id, new_name):
+        payload = {
+            'new_name': new_name
+        }
+
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id, data=data, api_token=self.api_token)
+        response = req.put()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def toggle_backend(self, backend_id, new_state):
+        payload = {
+            'newState': str(new_state)
+        }
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id, data=data, api_token=self.api_token)
+        response = req.post()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def list_sizes(self, backend_id):
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/sizes', api_token=self.api_token)
+        sizes = req.get().json()
+        return sizes
+
+    def list_locations(self, backend_id):
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/locations', api_token=self.api_token)
+        locations = req.get().json()
+        return locations
+
+    def list_keys(self):
+        req = RequestsHandler(self.uri+'/keys', api_token=self.api_token)
+        keys = req.get().json()
+        return keys
+
+    def add_key(self, key_name, private):
+        payload = {
+            'id': key_name,
+            'priv': private
+        }
+
+        data = json.dumps(payload)
+
+        req = RequestsHandler(self.uri+'/keys', data=data, api_token=self.api_token)
+        response = req.put()
+
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def delete_key(self, key_id):
+        req = RequestsHandler(self.uri+'/keys/'+key_id, api_token=self.api_token)
+        response = req.delete()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def list_images(self, backend_id):
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/images', api_token=self.api_token)
+        images = req.get().json()
+        return images
+
+    def search_image(self, backend_id, search_term):
+        payload = {
+            'search_term': search_term
+        }
+        data = json.dumps(payload)
+
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/images', api_token=self.api_token, data=data)
+        images = req.get().json()
+        return images
+
+    def list_machines(self, backend_id, filtered=None):
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/machines', api_token=self.api_token)
+        all_machines = req.get().json()
+        machines = []
+
+        if filtered == "running":
+            for machine in all_machines:
+                if machine['state'] == 'running':
+                    machines.append(machine)
+        else:
+            machines = all_machines
+        return machines
+
+    def create_machine(self, backend_id, name, key_id, image_id, location_id, size_id, image_extra="", disk=""):
+        payload = {
+            'name': name,
+            'key': key_id,
+            'image': image_id,
+            'location': location_id,
+            'size': size_id,
+            'image_extra': image_extra,
+            'disk': disk
+        }
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/machines', data=data, api_token=self.api_token)
+        response = req.post()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def machine_actions(self, backend_id, machine_id, action):
+        payload = {
+            'action': action
+        }
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/machines/'+machine_id, data=data,
+                              api_token=self.api_token)
+        response = req.post()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def toggle_monitoring(self, backend_id, machine_id, machine_name, public_ips, dns_name, action='enable'):
+        payload = {
+            'action': action,
+            'name': machine_name,
+            'public_ips': public_ips,
+            'dns_name': dns_name
+        }
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/monitoring', data=data,
+                              api_token=self.api_token)
+        response = req.post()
+        if response.ok:
+            return response
+        else:
+            return response.status_code
+
+    def get_stats(self, backend_id, machine_id, start, stop, step):
+        payload = {
+            'start': start,
+            'stop': stop,
+            'step': step
+        }
+
+        data = json.dumps(payload)
+        req = RequestsHandler(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/stats', data=data,
+                              api_token=self.api_token)
+        stats = req.get().json()
+        return stats
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup (name='mist.client',
+       version='0.0.1',
+       description='Python client for mist.io',
+       url='https://github.com/mistio/mist.client',
+       author= 'Chris Loukas',
+       author_email='commixon@gmail.com',
+       license='MIT',
+       packages=['mist'],
+       install_requires=[
+           'requests',
+       ],
+       zip_safe=False)
