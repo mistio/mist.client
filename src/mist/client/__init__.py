2610162c38ef011d0b01baf59e250535258a40d1
commixon <commixon@gmail.com>
2014-08-19 18:57:35 +0300

    Major update in backends, keys, machines

--- src/mist/client/__init__.py
+++ src/mist/client/__init__.py
@@ -1,9 +1,7 @@
 import json
 
-from time import time
-
 from mist.client.helpers import RequestsHandler
-from mist.client.model import Backend
+from mist.client.model import Backend, Key
 
 
 class MistClient(object):
@@ -29,7 +27,8 @@ class MistClient(object):
         self.api_token = None
         self.user_details = None
 
-        self.backends = {}
+        self._backends = None
+        self._keys = None
 
         if self.email and self.password:
             self.__authenticate()
@@ -60,6 +59,7 @@ class MistClient(object):
         """
         return RequestsHandler(*args, api_token=self.api_token, **kwargs)
 
+    @property
     def supported_providers(self):
         """
         Request a list of all available providers
@@ -71,7 +71,7 @@ class MistClient(object):
         supported_providers = providers['supported_providers']
         return supported_providers
 
-    def list_backends(self):
+    def _list_backends(self):
         """
         Request a list of all added backends.
 
@@ -81,10 +81,24 @@ class MistClient(object):
         """
         req = self.request(self.uri+'/backends')
         backends = req.get().json()
-        for backend in backends:
-            self.backends[backend['title']] = Backend(backend, self.api_token)
+        if backends:
+            for backend in backends:
+                self._backends[backend['title']] = Backend(backend, self)
+        else:
+            self._backends = {}
 
-        return self.backends.keys()
+    @property
+    def backends(self):
+        if self._backends is None:
+            self._backends = {}
+            self._list_backends()
+
+        return self._backends
+
+    def update_backends(self):
+        self._backends = {}
+        self._list_backends()
+        return self._backends
 
     def add_backend(self, title, provider, key, secret, tenant_name=None, region=None, apiurl=None, machine_ip=None,
                     machine_key=None, machine_user=None, compute_endpoint=None, machine_port=None):
@@ -122,162 +136,106 @@ class MistClient(object):
 
         req = self.request(self.uri+'/backends', data=json.dumps(payload))
         response = req.post()
-        self.list_backends()
+        self.update_backends()
         return
 
-    def delete_backend(self, backend_id):
-        req = self.request(self.uri + '/backends/' +backend_id)
-        response = req.delete()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def rename_backend(self, backend_id, new_name):
-        payload = {
-            'new_name': new_name
-        }
-
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id, data=data)
-        response = req.put()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def toggle_backend(self, backend_id, new_state):
-        payload = {
-            'newState': str(new_state)
-        }
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id, data=data)
-        response = req.post()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def list_sizes(self, backend_id):
-        req = self.request(self.uri+'/backends/'+backend_id+'/sizes')
-        sizes = req.get().json()
-        return sizes
-
-    def list_locations(self, backend_id):
-        req = self.request(self.uri+'/backends/'+backend_id+'/locations')
-        locations = req.get().json()
-        return locations
-
-    def list_keys(self):
+    def _list_keys(self):
         req = self.request(self.uri+'/keys')
         keys = req.get().json()
+        if keys:
+            self._keys = {}
+            for key in keys:
+                self._keys[key['id']] = Key(key, self)
+        else:
+            self._keys = {}
         return keys
 
-    def add_key(self, key_name, private):
-        payload = {
-            'id': key_name,
-            'priv': private
-        }
+    @property
+    def keys(self):
+        if self._keys is None:
+            self._keys = {}
+            self._list_keys()
 
-        data = json.dumps(payload)
+        return self._keys
 
-        req = self.request(self.uri+'/keys', data=data)
-        response = req.put()
-
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def delete_key(self, key_id):
-        req = self.request(self.uri+'/keys/'+key_id)
-        response = req.delete()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def list_images(self, backend_id):
-        req = self.request(self.uri+'/backends/'+backend_id+'/images')
-        images = req.get().json()
-        return images
-
-    def search_image(self, backend_id, search_term):
-        payload = {
-            'search_term': search_term
-        }
-        data = json.dumps(payload)
-
-        req = self.request(self.uri+'/backends/'+backend_id+'/images', data=data)
-        images = req.get().json()
-        return images
-
-    def list_machines(self, backend_id, filtered=None):
-        req = self.request(self.uri+'/backends/'+backend_id+'/machines')
-        all_machines = req.get().json()
-        machines = []
-
-        if filtered == "running":
-            for machine in all_machines:
-                if machine['state'] == 'running':
-                    machines.append(machine)
-        else:
-            machines = all_machines
-        return machines
-
-    def create_machine(self, backend_id, name, key_id, image_id, location_id, size_id, image_extra="", disk=""):
-        payload = {
-            'name': name,
-            'key': key_id,
-            'image': image_id,
-            'location': location_id,
-            'size': size_id,
-            'image_extra': image_extra,
-            'disk': disk
-        }
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id+'/machines', data=data)
-        response = req.post()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def machine_actions(self, backend_id, machine_id, action):
-        payload = {
-            'action': action
-        }
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id, data=data)
-        response = req.post()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def toggle_monitoring(self, backend_id, machine_id, machine_name, public_ips, dns_name, action='enable'):
-        payload = {
-            'action': action,
-            'name': machine_name,
-            'public_ips': public_ips,
-            'dns_name': dns_name
-        }
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/monitoring', data=data)
-        response = req.post()
-        if response.ok:
-            return response
-        else:
-            return response.status_code
-
-    def get_stats(self, backend_id, machine_id, start=int(time()), stop=int(time())+10, step=10):
-        payload = {
-            'start': start,
-            'stop': stop,
-            'step': step
-        }
-
-        data = json.dumps(payload)
-        req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/stats', data=data)
-        stats = req.get().json()
-        return stats
+    # def add_key(self, key_name, private):
+    #     payload = {
+    #         'id': key_name,
+    #         'priv': private
+    #     }
+    #
+    #     data = json.dumps(payload)
+    #
+    #     req = self.request(self.uri+'/keys', data=data)
+    #     response = req.put()
+    #
+    #     if response.ok:
+    #         return response
+    #     else:
+    #         return response.status_code
+    #
+    # def delete_key(self, key_id):
+    #     req = self.request(self.uri+'/keys/'+key_id)
+    #     response = req.delete()
+    #     if response.ok:
+    #         return response
+    #     else:
+    #         return response.status_code
+    #
+    #
+    # def create_machine(self, backend_id, name, key_id, image_id, location_id, size_id, image_extra="", disk=""):
+    #     payload = {
+    #         'name': name,
+    #         'key': key_id,
+    #         'image': image_id,
+    #         'location': location_id,
+    #         'size': size_id,
+    #         'image_extra': image_extra,
+    #         'disk': disk
+    #     }
+    #     data = json.dumps(payload)
+    #     req = self.request(self.uri+'/backends/'+backend_id+'/machines', data=data)
+    #     response = req.post()
+    #     if response.ok:
+    #         return response
+    #     else:
+    #         return response.status_code
+    #
+    # def machine_actions(self, backend_id, machine_id, action):
+    #     payload = {
+    #         'action': action
+    #     }
+    #     data = json.dumps(payload)
+    #     req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id, data=data)
+    #     response = req.post()
+    #     if response.ok:
+    #         return response
+    #     else:
+    #         return response.status_code
+    #
+    # def toggle_monitoring(self, backend_id, machine_id, machine_name, public_ips, dns_name, action='enable'):
+    #     payload = {
+    #         'action': action,
+    #         'name': machine_name,
+    #         'public_ips': public_ips,
+    #         'dns_name': dns_name
+    #     }
+    #     data = json.dumps(payload)
+    #     req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/monitoring', data=data)
+    #     response = req.post()
+    #     if response.ok:
+    #         return response
+    #     else:
+    #         return response.status_code
+    #
+    # def get_stats(self, backend_id, machine_id, start=int(time()), stop=int(time())+10, step=10):
+    #     payload = {
+    #         'start': start,
+    #         'stop': stop,
+    #         'step': step
+    #     }
+    #
+    #     data = json.dumps(payload)
+    #     req = self.request(self.uri+'/backends/'+backend_id+'/machines/'+machine_id+'/stats', data=data)
+    #     stats = req.get().json()
+    #     return stats
--- src/mist/client/model.py
+++ src/mist/client/model.py
@@ -1,3 +1,4 @@
+import json
 from mist.client.helpers import RequestsHandler
 
 
@@ -5,12 +6,157 @@ class Backend(object):
     """
 
     """
-    def __init__(self, backend, api_token=None):
+    def __init__(self, backend, mist_client):
+        self.mist_client = mist_client
+        self.info = backend
         self.title = backend['title']
         self.id = backend['id']
         self.enabled = backend['enabled']
         self.provider = backend['provider']
-        self.api_token = api_token
+        self.api_token = self.mist_client.api_token
+
+        self._machines = None
+
+    def __str__(self):
+        return "%s => %s:%s" % (self.__class__.__name__, self.title, self.id)
+
+    def __repr__(self):
+        return "%s => %s:%s" % (self.__class__.__name__, self.title, self.id)
+
+    def request(self, *args, **kwargs):
+        """
+        The main purpose of this is to be a wrapper-like function to pass the api_token and all the other params to the
+        requests that are being made
+
+        :returns: An instance of RequestsHandler
+        """
+        return RequestsHandler(*args, api_token=self.api_token, **kwargs)
+
+    def delete(self):
+        req = self.request(self.mist_client.uri + '/backends/' + self.id)
+        req.delete()
+        self.mist_client.update_backends()
+
+    def rename(self, new_name):
+        payload = {
+            'new_name': new_name
+        }
+        data = json.dumps(payload)
+        req = self.request(self.mist_client.uri + '/backends/' + self.id, data=data)
+        req.put()
+        self.title = new_name
+        self.mist_client.update_backends()
+
+    def enable(self):
+        payload = {
+            "new_state": "1"
+        }
+        data = json.dumps(payload)
+        req = self.request(self.mist_client.uri+'/backends/'+self.id, data=data)
+        req.post()
+        self.enabled = True
+        self.mist_client.update_backends()
+
+    def disable(self):
+        payload = {
+            "new_state": "0"
+        }
+        data = json.dumps(payload)
+        req = self.request(self.mist_client.uri+'/backends/'+self.id, data=data)
+        req.post()
+        self.enabled = False
+        self.mist_client.update_backends()
+
+    @property
+    def sizes(self):
+        req = self.request(self.mist_client.uri+'/backends/'+self.id+'/sizes')
+        sizes = req.get().json()
+        return sizes
+
+    @property
+    def locations(self):
+        req = self.request(self.mist_client.uri+'/backends/'+self.id+'/locations')
+        locations = req.get().json()
+        return locations
+
+    @property
+    def images(self):
+        req = self.request(self.mist_client.uri+'/backends/'+self.id+'/images')
+        images = req.get().json()
+        return images
+
+    def search_image(self, search_term):
+        payload = {
+            'search_term': search_term
+        }
+        data = json.dumps(payload)
+
+        req = self.request(self.mist_client.uri+'/backends/'+self.id+'/images', data=data)
+        images = req.get().json()
+        return images
+
+    def _list_machines(self):
+        req = self.request(self.mist_client.uri+'/backends/'+self.id+'/machines')
+        machines = req.get().json()
+
+        if machines:
+            for machine in machines:
+                self._machines[machine['name']] = Machine(machine, self)
+        else:
+            self._machines = {}
+
+    @property
+    def machines(self):
+        if self._machines is None:
+            self._machines = {}
+            self._list_machines()
+
+        return self._machines
+
+    def update_machines(self):
+        self._machines = {}
+        self._list_machines()
+        return self._machines
+
+
+class Machine(object):
+    def __init__(self, machine, backend):
+        self.backend = backend
+        self.mist_client = backend.mist_client
+        self.info = machine
+        self.api_token = self.mist_client.api_token
+        self.name = machine['name']
+        self.id = machine['id']
+
+    def __str__(self):
+        return "%s => %s:%s" % (self.__class__.__name__, self.name, self.id)
+
+    def __repr__(self):
+        return "%s => %s:%s" % (self.__class__.__name__, self.name, self.id)
+
+    def request(self, *args, **kwargs):
+        """
+        The main purpose of this is to be a wrapper-like function to pass the api_token and all the other params to the
+        requests that are being made
+
+        :returns: An instance of RequestsHandler
+        """
+        return RequestsHandler(*args, api_token=self.api_token, **kwargs)
+
+
+class Key(object):
+    def __init__(self, key, mist_client):
+        self.mist_client = mist_client
+        self.api_token = self.mist_client.api_token
+        self.id = key['id']
+        self.default_key = key['isDefault']
+        self.info = key
+
+    def __str__(self):
+        return "%s => %s" % (self.__class__.__name__, self.id)
+
+    def __repr__(self):
+        return "%s => %s" % (self.__class__.__name__, self.id)
 
     def request(self, *args, **kwargs):
         """
