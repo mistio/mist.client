0ca74c0ab1d24eb199af19b1c03069aa74466f87
Chris Loukas <commixon@gmail.com>
2014-11-14 10:28:40 +0200

    Update list-locations

--- src/mistcommand/helpers/locations.py
+++ src/mistcommand/helpers/locations.py
@@ -1,21 +1,29 @@
 import sys
 
 from prettytable import PrettyTable
-from mistcommand.helpers.backends import choose_backend
+from mistcommand.helpers.backends import return_backend
 from mistcommand.helpers.login import authenticate
 
-def list_locations(backend):
-    locations = backend.locations
-    x = PrettyTable(["Name", "ID"])
-    for location in locations:
-        x.add_row([location['name'], location['id']])
 
-    print x
+def list_locations(backend, pretty):
+    locations = backend.locations
+
+    if pretty:
+        x = PrettyTable(["Name", "ID"])
+        for location in locations:
+            x.add_row([location['name'], location['id']])
+
+        print x
+    else:
+        for location in locations:
+            print "%-20s %-20s" % (location['name'], location['id'])
 
 
 def location_action(args):
 
-    if args.action == 'list':
+    if args.action == 'list-locations':
         client = authenticate()
-        backend = choose_backend(client, args)
-        list_locations(backend)
+        backend = return_backend(client, args)
+        pretty = args.pretty
+
+        list_locations(backend, pretty)
