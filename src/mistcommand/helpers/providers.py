61fa750d4682828a82ae45cb05f51ec2b9a95e2b
Chris Loukas <commixon@gmail.com>
2014-11-14 10:01:40 +0200

    Update list-providers

--- src/mistcommand/helpers/providers.py
+++ src/mistcommand/helpers/providers.py
@@ -4,19 +4,24 @@ from prettytable import PrettyTable
 from mistcommand.helpers.login import authenticate
 
 
-def list_providers(client):
+def list_providers(client, pretty):
     providers = client.supported_providers
 
-    x = PrettyTable(["Title", "Provider ID"])
-    for provider in providers:
-        x.add_row([provider['title'], provider['provider']])
+    if pretty:
+        x = PrettyTable(["Title", "Provider ID"])
+        for provider in providers:
+            x.add_row([provider['title'], provider['provider']])
 
-    print x
+        print x
+    else:
+        for provider in providers:
+            print "%-30s %-20s" % (provider['title'], provider['provider'])
 
 
 def provider_action(args):
 
     client = authenticate()
 
-    if args.action == "list":
-        list_providers(client)
+    if args.action == "list-providers":
+        pretty = args.pretty
+        list_providers(client,pretty)
